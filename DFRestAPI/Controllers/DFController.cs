869c9e5a58047241668df6021cf83da7f7bf90a3
Akshay Deshpande <Akshay.Deshpande@wolterskluwer.com>
2018-12-10 17:19:07 +0530

    Added methods to create db/collection/documents

--- DFRestAPI/Controllers/DFController.cs
+++ DFRestAPI/Controllers/DFController.cs
@@ -10,11 +10,28 @@ namespace DFRestAPI.Controllers
     [RoutePrefix("dashboardframework")]
     public class DFController : ApiController
     {
-        [Route("dashboardframeworkrestapi")]
+        [Route("createdb")]
         [AcceptVerbs("GET")]
-        public IHttpActionResult GetDashboardFrameworkLayout()
+        public IHttpActionResult CreateDatabase()
         {
-            return  Ok(false); ;
+            CosmosDBHelper.CreateDB().Wait(5000);
+            return  Ok(true); 
+        }
+
+        [Route("createcollection")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult CreateCollection()
+        {
+            CosmosDBHelper.CreateCollection().Wait(5000);
+            return Ok(true);
+        }
+
+        [Route("createdocument")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult CreateDocument()
+        {
+            CosmosDBHelper.CreateDocument().Wait(5000);
+            return Ok(true);
         }
     }
 }
---
+++ DFRestAPI/CosmosDBHelper.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace DFRestAPI
+{
+    public class CosmosDBHelper
+    {
+        private static string DBName = ConfigurationManager.AppSettings["database"];
+        private static string CollectionName = ConfigurationManager.AppSettings["collection"];
+        public static Uri DatabaseUri => UriFactory.CreateDatabaseUri(DBName);
+        public static Uri CollectionUri => UriFactory.CreateDocumentCollectionUri(DBName, CollectionName);
+
+        private static string endPoint = ConfigurationManager.AppSettings["CosmosdbEndpoint"];
+        private static string masterKey = ConfigurationManager.AppSettings["CosmosDbMasterKey"];
+
+        public async static Task CreateDB()
+        {
+            using (var client = new DocumentClient(new Uri(endPoint), masterKey))
+            {
+                await CreateDatabase(client);
+            }
+        }
+
+        public async static Task CreateCollection()
+        {
+            using (var client = new DocumentClient(new Uri(endPoint), masterKey))
+            {
+                await CreateCollection(client, CollectionName);
+            }
+        }
+
+        public async static Task CreateDocument()
+        {
+            using (var client = new DocumentClient(new Uri(endPoint), masterKey))
+            {
+                await CreateDocuments(client);
+            }
+        }
+
+        private async static Task CreateDatabase(DocumentClient client)
+        {
+            Database database = new Database()
+            {
+                Id = DBName
+            };
+            await client.CreateDatabaseAsync(database);
+            
+           // Console.WriteLine("Database created ->" + result.Resource.Id);
+        }
+        private async static Task CreateCollection(DocumentClient client, string collectionId,
+            int reservedRUs = 400, string partitionKey = "/partitionKey")
+        {
+            var p = new PartitionKeyDefinition();
+            p.Paths.Add(partitionKey);
+
+            var collectionDef = new DocumentCollection
+            {
+                Id = collectionId,
+                PartitionKey = p
+            };
+
+            var options = new RequestOptions { OfferThroughput = reservedRUs };
+
+            var result = await client.CreateDocumentCollectionAsync(DatabaseUri, collectionDef, options);
+            var collection = result.Resource;
+
+           // Console.WriteLine("Collection created ->" + result.Resource.Id);
+
+            // ViewCollections(client);
+        }
+        private async static Task CreateDocuments(DocumentClient client)
+        {
+            dynamic newDocument = new
+            {
+                name = "Akshay Deshpande",
+                address = new
+                {
+                    addressType = "Main Office",
+                    location = new
+                    {
+                        city = "Pune"
+                    },
+                    PinCode = "411001"
+                }
+            };
+
+            var result = await client.CreateDocumentAsync(CollectionUri, newDocument);
+
+           // Console.WriteLine("Document created ->" + result.Resource.Id);
+        }
+    }
+}
