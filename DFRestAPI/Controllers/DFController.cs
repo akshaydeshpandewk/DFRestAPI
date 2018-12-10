8a846ff1656fb03a7470899c06881c1836af430a
Akshay Deshpande <Akshay.Deshpande@wolterskluwer.com>
2018-12-10 14:28:13 +0530

    Added to Repo

---
+++ DFRestAPI/Controllers/DFController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DFRestAPI.Controllers
+{
+    [RoutePrefix("dashboardframework")]
+    public class DFController : ApiController
+    {
+        [Route("dashboardframeworkrestapi")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetDashboardFrameworkLayout()
+        {
+            return  Ok(true); ;
+        }
+    }
+}
